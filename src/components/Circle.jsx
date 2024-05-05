e1e3ec515a0b808224580ad33bc49b8750a5ded1
Abhishek Patil <abishkpatil@gmail.com>
2024-05-06 02:10:09 +0530

    feat: add dark mode detection

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from "react";
+
 export const Circle = ({ color, number, x, y }) => {
+    const [isDarkModeActivated, setDarkModeActivated] = useState(false);
+    useEffect(() => {
+        let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if(isDarkMode){
+            setDarkModeActivated(true);
+        }else{
+            setDarkModeActivated(false);
+        }
+    }, [])
+
     return <>
-        <div style={{ backgroundColor: color, height: '100px', width: '100px', borderRadius: '100%', position:'absolute', left:`${x-50}px`, top:`${y-50}px`, display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <div style={{ backgroundColor: color, height: '100px', width: '100px', borderRadius: '100%', position: 'absolute', left: `${x - 50}px`, top: `${y - 50}px`, display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem', color: isDarkModeActivated ? 'white' : 'black' }}>
             {number}
         </div>
     </>
